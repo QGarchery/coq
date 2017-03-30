3c47bb59f0fee0434219a6372b69fc3b203d4c80
Garchery <garchery.quentin@gmail.com>
2017-03-31 00:17:16 +0200

    bugsssssss

--- tp7/tp7.v
+++ tp7/tp7.v
@@ -129,15 +129,117 @@ Proof.
   reflexivity.
 Qed.
 
-Inductive movable_from (b1 : board) : board -> Prop :=
-| startb : movable_from b1 b1
-| column b2 : (exists p : pos, inv_col b1 p = b2) ->
-              movable_from b1 b2
-| row b2 : (exists p : pos, inv_row b1 p = b2) ->
-           movable_from b1 b2.
 
-Lemma movable :
-  movable_from start target.
+Definition move (b1 b2 : board) : Prop :=
+exists p : pos, b2 = inv_col b1 p \/ b2 = inv_row b1 p.                                       
+
+Lemma inv_color_idem :
+  forall c : color,
+    inv_color (inv_color c) = c.
 
 Proof.
-  apply row.
+  intros.
+  destruct c; simpl; reflexivity.
+Qed.
+
+Lemma inv_row_idem:
+  forall b : board, forall p : pos,
+      inv_row (inv_row b p) p = b.
+
+
+Proof.
+  intros.
+  destruct b.
+  destruct p0.
+  destruct t0.
+  destruct p0.
+  destruct t1.
+  destruct p0.
+  destruct t.
+  destruct p0.
+  destruct p;
+  simpl;
+  rewrite inv_color_idem;
+  rewrite inv_color_idem;
+  rewrite inv_color_idem;
+  reflexivity.
+Qed.
+
+Lemma inv_col_idem:
+  forall b : board, forall p : pos,
+      inv_col (inv_col b p) p = b.
+
+
+Proof.
+  intros.
+  destruct b.
+  destruct p0.
+  destruct t0.
+  destruct p0.
+  destruct t1.
+  destruct p0.
+  destruct t.
+  destruct p0.
+  destruct p;
+  simpl;
+  rewrite inv_color_idem;
+  rewrite inv_color_idem;
+  rewrite inv_color_idem;
+  reflexivity.
+Qed.
+
+Lemma move_comm :
+  forall b1 b2 : board, 
+    move b1 b2 -> move b2 b1.
+
+Proof.
+  intros.
+  destruct H.
+  exists x.
+  destruct H.
+  left.
+  rewrite H.
+  rewrite inv_col_idem.
+  reflexivity.
+  right.
+  rewrite H.
+  rewrite inv_row_idem.
+  reflexivity.
+Qed.
+
+  
+Inductive moves : board -> board -> Prop :=
+| move_ax b1 : moves b1 b1
+| move_ind b1 b3: (exists b2 : board, moves b1 b2 /\ move b2 b3) -> moves b1 b3.
+
+
+
+Lemma trying :
+  forall b1 : board,
+    moves b1 b1.
+
+
+
+
+
+Lemma moves_comm:
+  forall b1 b2 : board,
+    moves b1 b2 -> moves b2 b1.
+
+Proof.
+  intros.
+
+  induction H eqn:?.
+  apply move_ax.
+  
+  
+
+
+Lemma movable :
+  moves start target.
+
+Proof.
+  assert (moves start start).
+  apply move_ax.
+  assert (moves start (inv_col start C)).
+  apply (move_ind (inv_col start C)).
